9b945f02a53ed22aa4d44d07d0ea9b51bb3e0aff
Turtle <turtle84375@programmer.net>
2022-05-15 09:11:35 +0000

    Remove unused import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, url_for
+from flask import Flask, request, make_response, redirect, render_template
 import hashlib
 import pymongo
 import os
