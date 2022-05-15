06544f20569860dd65dddc62e92b08adf08f680f
Turtle <turtle84375@programmer.net>
2022-05-15 08:26:42 +0000

    Working registration system

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ def login():
           return redirect('https://turtle84375.me/login?error=3')
         else:
           resp = make_response(render_template('success.html'))
-          resp.set_cookie('auth', x.get('username'), domain='.turtle84375.me', max_age=10)
+          resp.set_cookie('auth', x.get('username'), domain='.turtle84375.me', max_age=3600)
           if x.get('admin'):
-            resp.set_cookie('admin', 'true', domain='.turtle84375.me', max_age=10)
+            resp.set_cookie('admin', 'true', domain='.turtle84375.me', max_age=3600)
           return resp
     else:
         return redirect('https://turtle84375.me/login?error=1')
@@ -40,9 +40,30 @@ def register():
     client = pymongo.MongoClient(mongoURL)
     db = client["turtle84375-me"]
     col = db["users"]
-	
+
     user = request.form['username'].lower()
     pwrd = request.form['password']
+    pwrd = hashlib.sha256(pwrd.encode('utf-8')).hexdigest()
+
+    query = { "username": user }
+    global unique
+    unique = True
+    for x in col.find(query):
+      unique = False
+
+    if unique:
+      data = {
+		    'username': user,
+			  'password': pwrd,
+			  'admin': False,
+			  'banned': False
+		  }
+
+      x = col.insert_one(data)
+      print(x)
+      return redirect('https://turtle84375.me/login?error=0')
+    else:
+      return redirect('https://turtle84375.me/register?error=1')
 					 
 
 if __name__ == '__main__':
